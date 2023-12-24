4509239f7e92667e133f0c927ee9f38b5bb56216
Liu Zhongwei <liuzhongwei@espressif.com>
2023-12-24 19:10:58 +0800

    remove old

--- 86switch_NEW.ino
+++ 86switch_NEW.ino
@@ -49,28 +49,28 @@
    Seeeduino XIAO dev board    : CS:  3, DC:  2, RST:  1, BL:  0, SCK:  8, MOSI: 10, MISO:  9
    Teensy 4.1 dev board        : CS: 39, DC: 41, RST: 40, BL: 22, SCK: 13, MOSI: 11, MISO: 12
  ******************************************************************************/
-#include <Arduino_GFX_Library.h>
+// #include <Arduino_GFX_Library.h>
 
 #define GFX_BL 38
 
-Arduino_ESP32RGBPanel *bus = new Arduino_ESP32RGBPanel(
-    39 /* CS */, 48 /* SCK */, 47 /* SDA */,
-    18 /* DE */, 17 /* VSYNC */, 16 /* HSYNC */, 21 /* PCLK */,
-    11 /* R0 */, 12 /* R1 */, 13 /* R2 */, 14 /* R3 */, 0 /* R4 */,
-    8 /* G0 */, 20 /* G1 */, 3 /* G2 */, 46 /* G3 */, 9 /* G4 */, 10 /* G5 */,
-    4 /* B0 */, 5 /* B1 */, 6 /* B2 */, 7 /* B3 */, 15 /* B4 */
-);
-Arduino_ST7701_RGBPanel *gfx = new Arduino_ST7701_RGBPanel(
-    bus, GFX_NOT_DEFINED /* RST */, 0 /* rotation */,
-    true /* IPS */, 480 /* width */, 480 /* height */,
-    st7701_type1_init_operations, sizeof(st7701_type1_init_operations), true /* BGR */,
-    10 /* hsync_front_porch */, 8 /* hsync_pulse_width */, 50 /* hsync_back_porch */,
-    10 /* vsync_front_porch */, 8 /* vsync_pulse_width */, 20 /* vsync_back_porch */);
+// Arduino_ESP32RGBPanel *bus = new Arduino_ESP32RGBPanel(
+//     39 /* CS */, 48 /* SCK */, 47 /* SDA */,
+//     18 /* DE */, 17 /* VSYNC */, 16 /* HSYNC */, 21 /* PCLK */,
+//     11 /* R0 */, 12 /* R1 */, 13 /* R2 */, 14 /* R3 */, 0 /* R4 */,
+//     8 /* G0 */, 20 /* G1 */, 3 /* G2 */, 46 /* G3 */, 9 /* G4 */, 10 /* G5 */,
+//     4 /* B0 */, 5 /* B1 */, 6 /* B2 */, 7 /* B3 */, 15 /* B4 */
+// );
+// Arduino_ST7701_RGBPanel *gfx = new Arduino_ST7701_RGBPanel(
+//     bus, GFX_NOT_DEFINED /* RST */, 0 /* rotation */,
+//     true /* IPS */, 480 /* width */, 480 /* height */,
+//     st7701_type1_init_operations, sizeof(st7701_type1_init_operations), true /* BGR */,
+//     10 /* hsync_front_porch */, 8 /* hsync_pulse_width */, 50 /* hsync_back_porch */,
+//     10 /* vsync_front_porch */, 8 /* vsync_pulse_width */, 20 /* vsync_back_porch */);
 
 /*******************************************************************************
    Please config the touch panel in touch.h
  ******************************************************************************/
-#include "touch.h"
+// #include "touch.h"
 
 #include <esp_heap_caps.h>
 
@@ -90,7 +90,7 @@ void my_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color
 #if (LV_COLOR_16_SWAP != 0)
   // gfx->draw16bitBeRGBBitmap(area->x1, area->y1, (uint16_t *)&color_p->full, w, h);
 #else
-  gfx->draw16bitRGBBitmap(area->x1, area->y1, (uint16_t *)&color_p->full, w, h);
+  // gfx->draw16bitRGBBitmap(area->x1, area->y1, (uint16_t *)&color_p->full, w, h);
 #endif
 
   lv_disp_flush_ready(disp);
@@ -98,25 +98,25 @@ void my_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color
 
 void my_touchpad_read(lv_indev_drv_t *indev_driver, lv_indev_data_t *data)
 {
-  if (touch_has_signal())
-  {
-    if (touch_touched())
-    {
-      data->state = LV_INDEV_STATE_PR;
+  // if (touch_has_signal())
+  // {
+  //   if (touch_touched())
+  //   {
+  //     data->state = LV_INDEV_STATE_PR;
 
-      /*Set the coordinates*/
-      data->point.x = touch_last_x;
-      data->point.y = touch_last_y;
-    }
-    else if (touch_released())
-    {
+  //     /*Set the coordinates*/
+  //     data->point.x = touch_last_x;
+  //     data->point.y = touch_last_y;
+  //   }
+  //   else if (touch_released())
+  //   {
       data->state = LV_INDEV_STATE_REL;
-    }
-  }
-  else
-  {
-    data->state = LV_INDEV_STATE_REL;
-  }
+  //   }
+  // }
+  // else
+  // {
+  //   data->state = LV_INDEV_STATE_REL;
+  // }
 }
 
 Ticker ticker;
@@ -138,11 +138,11 @@ void setup()
   Serial.println("LVGL Widgets Demo");
 
   // Init touch device
-  touch_init();
+  // touch_init();
 
   // Init Display
-  gfx->begin(11000000);
-  gfx->fillScreen(BLACK);
+  // gfx->begin(11000000);
+  // gfx->fillScreen(BLACK);
 
 #ifdef GFX_BL
   // pinMode(GFX_BL, OUTPUT);
@@ -154,8 +154,8 @@ void setup()
 
   lv_init();
 
-  screenWidth = gfx->width();
-  screenHeight = gfx->height();
+  // screenWidth = gfx->width();
+  // screenHeight = gfx->height();
   // #ifdef ESP32
   // disp_draw_buf = (lv_color_t *)heap_caps_malloc(sizeof(lv_color_t) * screenWidth * 480, MALLOC_CAP_SPIRAM | MALLOC_CAP_8BIT);
   // #else
@@ -201,11 +201,11 @@ void setup()
     lv_disp_drv_register(&disp_drv);
 
     /* Initialize the (dummy) input device driver */
-    static lv_indev_drv_t indev_drv;
-    lv_indev_drv_init(&indev_drv);
-    indev_drv.type = LV_INDEV_TYPE_POINTER;
-    indev_drv.read_cb = my_touchpad_read;
-    lv_indev_drv_register(&indev_drv);
+    // static lv_indev_drv_t indev_drv;
+    // lv_indev_drv_init(&indev_drv);
+    // indev_drv.type = LV_INDEV_TYPE_POINTER;
+    // // indev_drv.read_cb = my_touchpad_read;
+    // lv_indev_drv_register(&indev_drv);
 
     // lv_demo_widgets();
     // lv_demo_benchmark();
@@ -218,7 +218,7 @@ void setup()
     ticker.attach(1, tcr1s);
     lv_timer_handler(); /* let the GUI do its work */
 
- 
+
   }
 }
 
